4610e341f1b11bb5e9978ccb9d37eb98858a72a6
Trung Hoang <trungiee@gmail.com>
2023-02-24 10:53:49 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+To get raspbian arm emulated on macosx intel
+
+# Challenges
+
+Need - test ansible playbooks on raspberry pi raspian os without hardware
+
+- Want to run headless raspian no UI
+- Qemu provides a way to run emulated arm raspbian on macosx x86. 
+- Rpi since 2022 doesn't provide default login
+- Qemu setup takes some effort
+- Docker image lukechilds/dockerpi hides away most of the qemu
+- Just need to setup a user so can login after
+
+# Howto
+
+1. docker pull lukechilds/dockerpi:vm
+2. download raspberry pi os lite image https://www.raspberrypi.com/software/operating-systems/#raspberry-pi-os-32-bit 
+3. use vagrant to mount image and write userconf file to setup a login user - vagrant up, run `sudo ./firstboot.sh .....img`
+4. docker run -it --mount type=bind,source="$(pwd)"/2023-02-21-raspios-bullseye-armhf-lite.img,target=/sdcard/filesystem.img  lukechilds/dockerpi:vm
+
+
---
+++ firstboot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Args
+img=$1
+
+#Usage checks
+if [[ -z $img ]]; then
+  echo "Usage: $0 imagefile.img"
+  exit -1
+fi
+if [[ ! -e $img ]]; then
+  echo "ERROR: $img is not a file..."
+  exit -2
+fi
+if (( EUID != 0 )); then
+   echo "ERROR: You need to be running as root."
+   exit -3
+fi
+
+#Creating the mount point
+losetup loop55 -P $img
+#Mounting image boot partition
+sudo mount /dev/loop55p1 /mnt
+#Creating username and password into userconfig file
+echo 'berryboot:$6$uE1YmGMLJIuYRDA6$7uggLlPOnrfoFczREehC9C.rHVe2N5QQfK5Tbuio1bRAgob6/6j1iN8QyrJkvv4rfVbhsFICI.cPmKe.rT8Qi.' > /mnt/userconf.txt #Adding custom user account only for Raspberry Pi OS Lite
+echo ""
+#Unmounting image boot partition
+sudo umount /mnt
+#Detaching the mount point
+sudo losetup -d /dev/loop55
+echo ""
+echo "-------------------------------------"
+echo "==== DONE! IMAGE READY ===="
+echo "-------------------------------------"
+echo ""
+sync
